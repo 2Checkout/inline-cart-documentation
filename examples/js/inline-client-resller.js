b0168c968ef59117c65094413296c6ac12a75d07
Dorian Floarea <dorian.floarea@2checkout.com>
2019-06-25 16:52:39 +0300

    Added setup + catalog product example.

---
+++ examples/js/inline-client-resller.js
@@ -0,0 +1,25 @@
+(function (document, src, libName, config) {
+    var script             = document.createElement('script');
+    script.src             = src;
+    script.async           = true;
+    var firstScriptElement = document.getElementsByTagName('script')[0];
+    script.onload          = function () {
+        for (var namespace in config) {
+            if (config.hasOwnProperty(namespace)) {
+                window[libName].setup.setConfig(namespace, config[namespace]);
+            }
+        }
+        window[libName].register();
+        window[libName].cart.setTest(true);
+    };
+
+    firstScriptElement.parentNode.insertBefore(script, firstScriptElement);
+})(
+    document,
+    'https://secure.2checkout.com/checkout/client/twoCoInlineCart.js',
+    'TwoCoInlineCart',
+    {
+        app : {merchant: 'AVLRNG'},
+        cart: {host: 'https://secure.2checkout.com'}
+    }
+);
